9568dd0ab9be4fc4c3fa4f0d0d6524adf06519b3
Erick Ricardo Lopez <162360754+ErickLopezQA@users.noreply.github.com>
2024-05-21 18:58:20 -0700

    first commit

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ let conn = mysql.createConnection(  // Create a connection to the database asign
     host : 'localhost',
     user : 'root',
     password : '',
-    database : 'articulos_api_nodejs',
+    database : 'prueba_tecnica',
   }
 );
 
@@ -28,16 +28,15 @@ conn.connect(function(error)  // Connect to the database and handle error
   }
 })
 
-
 app.get('/', function (req, res)  // Create a route to the root of the server and send a respons
 {
   res.send('Funcionando!');
 });
 
-//Show all the articles 
-app.get('/api/articulos', (req, res) => // Create a route to show all the articles
+//Show all the clients 
+app.get('/api/clientes', (req, res) => // Create a route to show all the clients
 {
-  conn.query ('SELECT * FROM articulos', (error, filas) =>   // Query the database and handle error
+  conn.query ('SELECT * FROM clientes', (error, filas) =>   // Query the database and handle error
     {
     if (error)
       {
@@ -51,9 +50,9 @@ app.get('/api/articulos', (req, res) => // Create a route to show all the articl
 });
 
 //Show an article by id
-app.get('/api/articulos/:id', (req, res) => // Create a route to show an article by id
+app.get('/api/clientes/:id', (req, res) => // Create a route to show an article by id
 {
-  conn.query ('SELECT * FROM articulos WHERE id = ?', [req.params.id], (error, fila) =>   // Query the database and handle the error
+  conn.query ('SELECT * FROM clientes WHERE id_cliente = ?', [req.params.id], (error, fila) =>   // Query the database and handle the error
     {
     if (error)
       {
@@ -66,11 +65,11 @@ app.get('/api/articulos/:id', (req, res) => // Create a route to show an article
   })
 });
 
-//Create articles 
-app.post('/api/articulos', (req, res) => // Create a route to create articles
+//Create clients 
+app.post('/api/clientes', (req, res) => // Create a route to create clients
 {
-  let data = {descripcion:req.body.descripcion, precio:req.body.precio, stock:req.body.stock}; // Create a variable data with the values of the body
-  let sql = "INSERT INTO articulos SET ?";
+  let data = {nombre:req.body.nombre, apellido:req.body.apellido, correo:req.body.correo, telefono:req.body.telefono, direccion:req.body.direccion}; // Create a variable data with the values of the body
+  let sql = "INSERT INTO clientes SET ?";
   conn.query(sql, data, function(error, results) // Query the database and handle error
   {
     if (error)
@@ -84,15 +83,17 @@ app.post('/api/articulos', (req, res) => // Create a route to create articles
   });
 });
 
-//Update articles
-app.put('/api/articulos/:id', (req, res) => // Create a route to update articles
+//Update clients
+app.put('/api/clientes/:id', (req, res) => // Create a route to update clients
 {
-  let id = req.params.id;
-  let descripcion = req.body.descripcion;
-  let precio = req.body.precio;
-  let stock = req.body.stock;
-  let sql = "UPDATE articulos SET descripcion = ?, precio = ?, stock = ? WHERE id = ?";
-  conn.query(sql, [descripcion, precio, stock, id], function(error, results)
+  let id_cliente = req.params.id;
+  let nombre = req.body.nombre;
+  let apellido = req.body.apellido;
+  let correo = req.body.correo;
+  let telefono = req.body.telefono;
+  let direccion = req.body.direccion;
+  let sql = "UPDATE clientes SET nombre = ?, apellido = ?, correo = ?, telefono = ?, direccion = ? WHERE id_cliente = ?";
+  conn.query(sql, [nombre, apellido, correo, telefono, direccion, id_cliente], function(error, results)
   {
     // console.log(stock);
     if (error)
@@ -106,10 +107,96 @@ app.put('/api/articulos/:id', (req, res) => // Create a route to update articles
   });
 });
 
-//Delete articles
-app.delete('/api/articulos/:id', (req, res) =>  // Create a route to delete articles
+//Delete clients
+app.delete('/api/clientes/:id', (req, res) =>  // Create a route to delete clients
   {
-    conn.query("DELETE FROM articulos WHERE id = ?", [req.params.id], function(error, filas) // Query the database and handle error
+    conn.query("DELETE FROM clientes WHERE id_cliente = ?", [req.params.id], function(error, filas) // Query the database and handle error
+    {
+      if (error)
+      {
+        throw error;
+      }
+      else
+      {
+        res.send(filas); // Send the results to the client
+      }
+    });
+});
+
+//Show all the amounts
+app.get('/api/montos', (req, res) => // Create a route to show all the amounts
+{
+  conn.query ('SELECT * FROM montos', (error, filas) =>   // Query the database and handle error
+    {
+    if (error)
+      {
+        throw error;  
+      } 
+      else 
+      {
+        res.send(filas);  // Send the rows to the client
+      }
+  })
+});
+
+//Show an amount by id
+app.get('/api/montos/:id', (req, res) => // Create a route to show an amount by id
+{
+  conn.query ('SELECT * FROM montos WHERE id_monto = ?', [req.params.id], (error, fila) =>   // Query the database and handle the error
+    {
+    if (error)
+      {
+        throw error;  
+      } 
+      else 
+      {
+        res.send(fila);  // Send the rows to the client
+      }
+  })
+});
+
+//Create amounts 
+app.post('/api/montos', (req, res) => // Create a route to create amounts
+{
+  let data = {montos:req.body.montos}; // Create a variable data with the values of the body
+  let sql = "INSERT INTO montos SET ?";
+  conn.query(sql, data, function(error, results) // Query the database and handle error
+  {
+    if (error)
+    {
+      throw error;
+    }
+    else
+    {
+      res.send(results); // Send the results to the client
+    }
+  });
+});
+
+//Update amounts
+app.put('/api/montos/:id', (req, res) => // Create a route to update amounts
+{
+  let id_monto = req.params.id;
+  let montos = req.body.montos;
+  let sql = "UPDATE montos SET montos = ? WHERE id_monto = ?";
+  conn.query(sql, [montos, id_monto], function(error, results)
+  {
+    // console.log(stock);
+    if (error)
+    {
+      throw error;
+    }
+    else
+    {
+      res.send(results);
+    }
+  });
+});
+
+//Delete amounts
+app.delete('/api/montos/:id', (req, res) =>  // Create a route to delete amounts
+  {
+    conn.query("DELETE FROM montos WHERE id_monto = ?", [req.params.id], function(error, filas) // Query the database and handle error
     {
       if (error)
       {
@@ -122,7 +209,175 @@ app.delete('/api/articulos/:id', (req, res) =>  // Create a route to delete arti
     });
   });
 
-// const puerto = process.env.PUERTO; // Get the port from the environment variable PUERTO or use 3000 as default 
+//Show all the plazos 
+app.get('/api/plazos', (req, res) => // Create a route to show all the plazos
+{
+  conn.query ('SELECT * FROM plazos', (error, filas) =>   // Query the database and handle error
+    {
+    if (error)
+      {
+        throw error;  
+      } 
+      else 
+      {
+        res.send(filas);  // Send the rows to the plazos
+      }
+  })
+});
+
+//Show an plazos by id
+app.get('/api/plazos/:id', (req, res) => // Create a route to show an plazos by id
+{
+  conn.query ('SELECT * FROM plazos WHERE id_plazo = ?', [req.params.id], (error, fila) =>   // Query the database and handle the error
+    {
+    if (error)
+      {
+        throw error;  
+      } 
+      else 
+      {
+        res.send(fila);  // Send the rows to the client
+      }
+  })
+});
+
+//Create plazos 
+app.post('/api/plazos', (req, res) => // Create a route to create plazos
+{
+  let data = {plazos:req.body.plazos}; // Create a variable data with the values of the body
+  let sql = "INSERT INTO plazos SET ?";
+  conn.query(sql, data, function(error, results) // Query the database and handle error
+  {
+    if (error)
+    {
+      throw error;
+    }
+    else
+    {
+      res.send(results); // Send the results to the client
+    }
+  });
+});
+
+//Update plazos
+app.put('/api/plazos/:id', (req, res) => // Create a route to update plazos
+{
+  let id_plazo = req.params.id;
+  let plazos = req.body.plazos;
+
+  let sql = "UPDATE plazos SET plazos = ? WHERE id_plazo = ?";
+  conn.query(sql, [plazos, id_plazo], function(error, results)
+  {
+    // console.log(stock);
+    if (error)
+    {
+      throw error;
+    }
+    else
+    {
+      res.send(results);
+    }
+  });
+});
+
+//Delete plazos
+app.delete('/api/plazos/:id', (req, res) =>  // Create a route to delete plazos
+  {
+    conn.query("DELETE FROM plazos WHERE id_plazo = ?", [req.params.id], function(error, filas) // Query the database and handle error
+    {
+      if (error)
+      {
+        throw error;
+      }
+      else
+      {
+        res.send(filas); // Send the results to the plazo
+      }
+    });
+  });
+
+  //Show all the prestamos 
+  app.get('/api/prestamos', (req, res) => {
+    conn.query('CALL ObtenerPrestamos()', (error, results) => {
+        if (error) {
+            console.error('Error executing stored procedure:', error.stack);
+            res.status(500).send('Error executing query');
+            return;
+        }
+        res.send(results[0]);  // Send the first result set from the stored procedure
+    });
+});
+
+
+//Show an prestamos by id
+app.get('/api/prestamos/:id', (req, res) => {
+  // Create a route to show a prestamo by id
+  conn.query('CALL obtener_prestamo(?)', [req.params.id], (error, results) => {
+    // Query the database and handle the error
+    if (error) {
+      throw error;
+    } else {
+      res.send(results[0]); // Send the first result set (rows) to the client
+    }
+  });
+});
+
+//Create prestamos 
+app.post('/api/prestamos', (req, res) => // Create a route to create prestamos
+{
+  let data = {id_cliente:req.body.id_cliente, id_monto:req.body.id_monto, id_plazo:req.body.id_plazo}; // Create a variable data with the values of the body
+  let sql = "INSERT INTO prestamos SET ?";
+  conn.query(sql, data, function(error, results) // Query the database and handle error
+  {
+    if (error)
+    {
+      throw error;
+    }
+    else
+    {
+      res.send(results); // Send the results to the prestamos
+    }
+  });
+});
+
+//Update prestamos
+app.put('/api/prestamos/:id', (req, res) => // Create a route to update prestamos
+{
+  let id_prestamo = req.params.id;
+  let id_cliente = req.body.id_cliente;
+  let id_monto = req.body.id_monto;
+  let id_plazo = req.body.id_plazo;
+  
+  let sql = "UPDATE prestamos SET id_cliente = ?, id_monto = ?, id_plazo = ? WHERE id_prestamo = ?";
+  conn.query(sql, [id_cliente, id_monto, id_plazo, id_prestamo], function(error, results)
+  {
+    // console.log(stock);
+    if (error)
+    {
+      throw error;
+    }
+    else
+    {
+      res.send(results);
+    }
+  });
+});
+
+//Delete prestamos
+app.delete('/api/prestamos/:id', (req, res) =>  // Create a route to delete prestamos
+  {
+    conn.query("DELETE FROM prestamos WHERE id_prestamo = ?", [req.params.id], function(error, filas) // Query the database and handle error
+    {
+      if (error)
+      {
+        throw error;
+      }
+      else
+      {
+        res.send(filas); // Send the results to the plazo
+      }
+    });
+  });
 
 app.listen (3000, function ()  // Print a message to the console
 {
